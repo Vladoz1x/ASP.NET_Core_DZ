5b3915070fdba32c9b69cb85115d420b7316539a
Vlad <128002193+Vladoz1x@users.noreply.github.com>
2025-11-15 14:32:31 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using WeatherApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton(new WeatherService("9cae67b57a369c614bfcf2657bab628e"));
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapControllers();
+app.MapGet("/", context => {
+    context.Response.Redirect("/index.html");
+    return Task.CompletedTask;
+});
+
+app.Run();
